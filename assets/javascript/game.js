3dba350bf324fb3f10dfd5230282044afcd98a8f
wendym <ozmabeam@gmail.com>
2019-05-13 18:35:37 -0500

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // <!-- Array -->
-var letters = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]; -->
+var letters = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
 
 // <!-- for loop -->
 for (var i = 0; i < letters.length; i++){
