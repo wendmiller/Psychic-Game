b03352ea1c617939a37f889cf8121097872b6d95
wendym <ozmabeam@gmail.com>
2019-05-12 21:40:01 -0500

    pseudo-code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,13 @@
 // <!-- Array -->
-var alphabet = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]; -->
+var letters = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]; -->
 
 // <!-- for loop -->
-for (var i = 0; i < alphabet.lenght; i++){
+for (var i = 0; i < letters.length; i++){
 }
 
+// 1 computer pick a letter
+// 2 allow 9 choices
+// 3 count down guesses under guess Header
+// 4 loss counter - define loss
+// 5 win counter - 
 
